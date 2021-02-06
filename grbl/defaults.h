8db00043e897600715120fbf12143a6fec175bad
q-ford <quinton.ford25@gmail.com>
2021-02-06 10:24:22 -0600

    Update default speed/acc and stepper idle time

--- grbl/defaults.h
+++ grbl/defaults.h
@@ -587,12 +587,12 @@
   #define DEFAULT_X_STEPS_PER_MM (MICROSTEPS_X*STEP_PER_REV_X/MM_PER_REV_X)
   #define DEFAULT_Y_STEPS_PER_MM (MICROSTEPS_Y*STEP_PER_REV_Y/MM_PER_REV_Y)
   #define DEFAULT_Z_STEPS_PER_MM (MICROSTEPS_Z*STEP_PER_REV_Z/MM_PER_REV_Z)
-  #define DEFAULT_X_MAX_RATE 500.0 // mm/min
-  #define DEFAULT_Y_MAX_RATE 500.0 // mm/min
-  #define DEFAULT_Z_MAX_RATE 500.0 // mm/min
-  #define DEFAULT_X_ACCELERATION (10.0*60*60) // 10*60*60 mm/min^2 = 10 mm/sec^2
-  #define DEFAULT_Y_ACCELERATION (10.0*60*60) // 10*60*60 mm/min^2 = 10 mm/sec^2
-  #define DEFAULT_Z_ACCELERATION (10.0*60*60) // 10*60*60 mm/min^2 = 10 mm/sec^2
+  #define DEFAULT_X_MAX_RATE 7620.0 // mm/min
+  #define DEFAULT_Y_MAX_RATE 7620.0 // mm/min
+  #define DEFAULT_Z_MAX_RATE 2540.0 // mm/min
+  #define DEFAULT_X_ACCELERATION (500.0*60*60) // 10*60*60 mm/min^2 = 10 mm/sec^2
+  #define DEFAULT_Y_ACCELERATION (500.0*60*60) // 10*60*60 mm/min^2 = 10 mm/sec^2
+  #define DEFAULT_Z_ACCELERATION (500.0*60*60) // 10*60*60 mm/min^2 = 10 mm/sec^2
   #define DEFAULT_X_MAX_TRAVEL 1219.2 // mm NOTE: Must be a positive value.
   #define DEFAULT_Y_MAX_TRAVEL 1219.2 // mm NOTE: Must be a positive value.
   #define DEFAULT_Z_MAX_TRAVEL 127.0 // mm NOTE: Must be a positive value.
@@ -601,7 +601,7 @@
   #define DEFAULT_STEP_PULSE_MICROSECONDS 10
   #define DEFAULT_STEPPING_INVERT_MASK 0
   #define DEFAULT_DIRECTION_INVERT_MASK 0
-  #define DEFAULT_STEPPER_IDLE_LOCK_TIME 25 // msec (0-254, 255 keeps steppers enabled)
+  #define DEFAULT_STEPPER_IDLE_LOCK_TIME 255 // msec (0-254, 255 keeps steppers enabled)
   #define DEFAULT_STATUS_REPORT_MASK 1 // MPos enabled
   #define DEFAULT_JUNCTION_DEVIATION 0.01 // mm
   #define DEFAULT_ARC_TOLERANCE 0.002 // mm
